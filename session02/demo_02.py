566c4ae5441166993de6f376578b7dcaf44babab
Zhi Li <zli@babson.edu>
2020-01-23 16:26:09 -0500

    finish session 02

---
+++ session02/demo_02.py
@@ -0,0 +1,57 @@
+print('Hello, world!')
+
+print('Hey Jude, don\'t make it bad')
+print("Hey Jude, don't make it bad")
+
+print('1 + 2 + 3 =', 1+2+3)
+
+
+# name = input()
+
+# print(name)
+
+# message = 'I did something cool today!'
+# print(message)
+# print('message')
+
+
+first_name = 'John'
+last_name = 'Lennon'
+print(first_name +' '+ last_name)
+
+print('Naah, na na nanana naah, nanana naah,\t hey Jude. \n' * 10)
+
+name = 'Kenzi'
+print(f'Hello, {name}!')
+
+print(f'Hello, {first_name} {last_name}!')
+
+print()
+print()
+
+pi = 3.1415926
+
+print(f'Pi equals {pi:.5f}.')
+print(f'Pi equals {pi:8.5f}.')
+print(f'Pi equals {pi:8.2f}.')
+
+print()
+print()
+year = 255
+print(f'{year}')
+print(f'{year:b}') 
+print(f'{year:x}')
+print(f'{year:e}')
+
+print()
+print()
+
+s1 = 'a'
+s2 = 'ab'
+s3 = 'abc'
+s4 = 'abcd'
+
+print(f'{s1:>10}')
+print(f'{s2:>10}')
+print(f'{s3:>10}')
+print(f'{s4:>10}')
