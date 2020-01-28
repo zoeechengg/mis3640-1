756044a4b99f84f53b2dfd6b1fadd7528206a73a
Zhi Li <zli@babson.edu>
2020-01-28 16:59:13 -0500

    Create demo.py

---
+++ session03/demo.py
@@ -0,0 +1,26 @@
+# name = 'Chris'
+# score = 95
+# d = {'ray': 90, 'Ivy': 91, name: score}
+
+
+
+
+"""
+Strings
+""" 
+# print('I\'m \"ok\".')
+
+# print('I\'m learning\nPython.')
+
+# import math
+# import pprint
+# pprint.pprint(dir(math)) 
+
+age = input('What is your age?')
+
+age = float(age)
+
+if age >= 21:
+    print('Yes, you can.')
+else:
+    print('Sorry, you cannot.')
