33ec31b5bc04897a128469cf3306538883ba7c4f
Zhi Li <zli@babson.edu>
2020-01-30 14:52:01 -0500

    Create ex_3.py

---
+++ session03/ex_3.py
@@ -0,0 +1,14 @@
+# Exercise 3
+
+import time
+
+print(time.time())
+current = time.time()
+second = current % 60
+minutes = (current // 60) % 60
+hours = (current // 60) // 60 % 24
+days = current // 60 // 60 // 24
+
+print(
+    f"Current time: {int(days):d} days, {int(hours):d} hours, {int(minutes):d} minutes and {second:.2f} seconds from Epoch."
+)
