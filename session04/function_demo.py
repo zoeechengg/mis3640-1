5163691fe96d22407c0e192183312d02851b4a36
Zhi Li <zli@babson.edu>
2020-01-31 09:24:02 -0500

    finish session 04

---
+++ session04/function_demo.py
@@ -0,0 +1,56 @@
+def print_lyrics():
+    print("Hey Jude. Don't make it bad.")
+    print("Take a sad song and make it better.")
+
+
+# print_lyrics()
+# print(type(print_lyrics))
+
+def repeat_lyrics():
+    print_lyrics()
+    print('Na - na - na - na - na, na - na - na - na')
+    print_lyrics()
+
+
+# repeat_lyrics()
+
+
+def print_twice(w):
+    print(w)
+    print(w)
+
+
+# print_twice('Babson')
+
+# my_name = 'Andrew'
+# print_twice(my_name)
+
+def cat_twice(part1, part2):
+    cat = part1 + part2
+    print_twice(cat)
+
+# cat_twice('Babson', 'College')
+
+# print(cat) 
+
+# cat_twice(1, 2) 
+# cat_twice('1', '2') 
+# cat_twcie(1, '2')
+
+
+def give_me_a_break():
+    str1 = 'break'
+    return str1 
+
+
+# give_me_a_break()
+# print(give_me_a_break())
+
+# print_twice(give_me_a_break()) 
+
+def give_me_two_breaks():
+    str1 = 'break'
+    return str1
+    print('another break') 
+
+print(give_me_two_breaks()) 
---
+++ session04/my_abs.py
@@ -0,0 +1,10 @@
+def my_abs(n):
+    if n < 0:
+        n = -n 
+        print(n)
+    else:
+        print(n)
+
+my_abs(-1)
+my_abs(0)
+my_abs(10)
