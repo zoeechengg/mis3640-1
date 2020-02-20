36dd3e31cc8da7a84116150ed1f67bd3b2a0ceb0
Zhi Li <zli@babson.edu>
2020-02-20 14:47:31 -0500

    Create word.py

---
+++ session09/word.py
@@ -0,0 +1,136 @@
+def find_long_words():
+    """
+    prints only the words with more than 20 characters
+    """
+    pass
+
+
+# find_long_words()
+
+
+def has_no_e(word):
+    """
+    returns True if the given word doesn’t have the letter "e" in it
+    """
+    pass
+
+
+# print(has_no_e('Babson'))
+# print(has_no_e('College'))
+# print(has_no_e('EA sports'))
+
+
+def find_words_no_e():
+    """
+    returns the percentage of the words that don't have the letter "e"
+    """
+    pass
+
+
+# perc_no_e = find_words_no_e()
+# print(f'The percentage of the words with no "e" is {perc_no_e:.2f}%.')
+
+
+def avoids(word, forbidden):
+    """
+    returns True if the given word does not use any of the forbidden letters
+    """
+    pass
+
+
+# print(avoids('Babson', 'abcde'))
+# print(avoids('College', 'e'))
+# print(avoids('Boston', 'xyz'))
+
+
+def find_words_no_vowels():
+    """
+    returns the percentage of the words that don't vowel letters
+    """
+    pass
+
+
+# perc_no_vowel = find_words_no_vowels()
+# print(f'The percentage of the words without vowel letters is {perc_no_vowel:.2f}%.')
+
+
+def uses_only(word, available):
+    """
+    takes a word and a string of letters, and that returns True if the word
+    contains only letters in the list.
+    """
+    pass
+
+
+# print(uses_only('Babson', 'aBbsonxyz'))
+# print(uses_only('college', 'aBbsonxyz'))
+
+
+def find_words_only_use_planet():
+    pass
+
+
+# print('Number of words that use only letters from "planet" is', find_words_only_use_planet())
+
+
+def uses_all(word, required):
+    """
+    takes a word and a string of required letters, and that returns True if
+    the word uses all the required letters at least once.
+    """
+    pass
+
+
+# please write test cases
+
+
+def find_words_using_all_vowels():
+    """
+    return the number of the words that use all the vowel letters
+    """
+    pass
+
+
+# print('The number of words that use all the vowels:', find_words_using_all_vowels())
+
+
+def is_abecedarian(word):
+    """
+    returns True if the letters in a word appear in alphabetical order
+    (double letters are ok).
+    """
+    pass
+
+
+# print(is_abecedarian('abs'))
+# print(is_abecedarian('college'))
+
+
+def find_abecedarian_words():
+    """
+    returns the number of abecedarian words
+    """
+    pass
+
+
+# print(find_abecedarian_words())
+
+
+def is_abecedarian_using_recursion(word):
+    """
+    returns True if the letters in a word appear in alphabetical order
+    (double letters are ok).
+    """
+    pass
+
+
+# print(is_abecedarian_using_recursion('abcdef'))
+
+
+def is_abecedarian_using_while(word):
+    """
+    returns True if the letters in a word appear in alphabetical order
+    (double letters are ok).
+    """
+    pass
+
