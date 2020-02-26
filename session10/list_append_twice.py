2bf8f8ba67c5a981c36c6b2c3c408bbf029a4b3b
Zhi Li <lzblack@users.noreply.github.com>
2020-02-26 16:57:28 -0500

    Create list_append_twice.py

---
+++ session10/list_append_twice.py
@@ -0,0 +1,30 @@
+def append_twice(a_list, val):
+    """Mutates arguments."""
+    a_list.append(val)
+    a_list.append(val)
+
+
+# nums = [1, 2, 3]
+# append_twice(nums, 4)
+# print(nums)
+
+
+def append_twice_bad(a_list, val):
+    """Useless!"""
+    a_list = a_list + [val, val]
+
+
+# nums = [1, 2, 3]
+# append_twice_bad(nums, 4)
+# print(nums)
+
+
+def append_twice_good(a_list, val):
+    """Returns a new list"""
+    a_list = a_list + [val, val]
+    return a_list
+
+
+# nums = [1, 2, 3]
+# new_nums = append_twice_good(nums, 4)
+# print(new_nums)
